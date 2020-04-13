734e15cf70a74366e97544e4fbb0b4c9af2b3d47
Mayur Salunkhe <msalunkhe008@gmail.com>
2020-04-13 22:33:50 +0530

    End of sectoin 11 - Photo upload feature

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -13,6 +13,7 @@ import { AuthService } from 'src/app/_services/auth.service';
 })
 export class MemberEditComponent implements OnInit {
   user: User;
+  photoUrl: string;
   @ViewChild('editForm') editForm: NgForm;
   // prevent closing browser
   @HostListener('window:beforeunload', ['$event'])
@@ -31,6 +32,7 @@ export class MemberEditComponent implements OnInit {
     this.route.data.subscribe(data => {
       this.user = data['user'];
     });
+    this.authService.currentPhotoUrl.subscribe(photoUrl => this.photoUrl = photoUrl)
   }
 
   updateUser() {
@@ -42,6 +44,10 @@ export class MemberEditComponent implements OnInit {
         this.alertify.error(error);
       });
 
+  }
+
+  updateMainPhoto(photoUrl) {
+    this.user.photoUrl = photoUrl;
   }
 
 }
